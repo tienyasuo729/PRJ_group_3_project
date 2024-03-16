b4c141316eb0ed9db9d97ddcf30a5beb92afcccd
tienyasuo729 <tienngqueson@gmail.com>
2024-03-16 20:14:44 +0700

    code

--- Shipping_Services_For_Everyone/src/main/java/org/example/shipping_services_for_everyone/model/Shipper.java
+++ Shipping_Services_For_Everyone/src/main/java/org/example/shipping_services_for_everyone/model/Shipper.java
@@ -1,10 +1,19 @@
 package org.example.shipping_services_for_everyone.model;
 
 public class Shipper {
+    private int idShipper;
     private People people;
     private ImageIdentification imageIdentification;
 
     public Shipper() {
+    }
+
+    public int getIdShipper() {
+        return idShipper;
+    }
+
+    public void setIdShipper(int idShipper) {
+        this.idShipper = idShipper;
     }
 
     public Shipper(People people, ImageIdentification imageIdentification) {
