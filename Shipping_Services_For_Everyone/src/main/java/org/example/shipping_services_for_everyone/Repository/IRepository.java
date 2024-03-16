74b5a9229e55f55ebad0b4eadbb931d917f91fde
tienyasuo729 <tienngqueson@gmail.com>
2024-03-16 10:16:54 +0700

    code

--- Shipping_Services_For_Everyone/src/main/java/org/example/shipping_services_for_everyone/Repository/IRepository.java
+++ Shipping_Services_For_Everyone/src/main/java/org/example/shipping_services_for_everyone/Repository/IRepository.java
@@ -1,6 +1,14 @@
 package org.example.shipping_services_for_everyone.Repository;
 
+import java.util.List;
+
 public interface IRepository<T> {
-    public void addNew(T shipper);
+    List<T> display(T object);
+    void addNew(T object);
+    void edit(T object);
+    void delete(T object);
+    List<T> find(T object);
+    Boolean checkIdIsExist(T object);
+
 }
 
